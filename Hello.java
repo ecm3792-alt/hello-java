83f7470527f5c1e7d24d2b142157e8675f67f1fc
Cowan Moorer <emoorer@samford.edu>
2026-04-13 09:10:39 -0500

    Added 2nd and 3rd print statement

--- Hello.java
+++ Hello.java
@@ -2,5 +2,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("Welcome to Java programming.");
+        System.out.println("Du Du Du Du Du, Max Verstaapen");
     }
 }
